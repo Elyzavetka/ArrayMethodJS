47f5a37c41167a6315a1dc8ec761c61d2c615919
Elyzavetka <lizamybella@gmail.com>
2023-01-16 15:34:33 +0000

    Update ArrayMethodsJS_1.js

--- ArrayMethodsJS_1.js
+++ ArrayMethodsJS_1.js
@@ -1,3 +1,5 @@
+//unshift
+
 const myArray = [1, 2, 3];
 console.log(myArray);
 
@@ -8,3 +10,15 @@ console.log(myArray);
 myArray.unshift(`abc`);
 
 console.log(myArray);
+
+//shift
+
+const myArray_1 = [1, 2, 3];
+console.log(myArray_1);
+
+myArray_1.shift();
+
+const removedElement = myArray_1.shift();
+
+console.log(myArray_1);
+console.log(removedElement);
